9c5278ee3ef3a4a315cec3e57d772c78bdedd93d
BiggestChris <stylianouchristopher8@gmail.com>
2025-01-10 06:38:35 +0000

    Updates to score-checking algorithm build

--- check_score.py
+++ check_score.py
@@ -18,6 +18,81 @@ array_1 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 array_2 = [[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1]]
 array_3 = [[1,1,1,1],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
+new_array = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+
+'''
+  A B C D E F G
+1
+2
+3
+4
+5
+6
+
+'''
+
+
+
+
+# 6 rows, 7 columns - arrays within arrays are columns within rows
+
+score_row = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]       # starts at A1-D1
+score_column = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]    # starts at A1-A4
+score_diagonal_up_right = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]                 # starts at A4-D1
+score_diagonal_down_right = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]               # starts at A1-D4
+
+i = 6
+j = 7
+
+# populate score_row
+
+for col_num in range((j - 4) + 1):
+    for row_num in range(i):
+        score_row[col_num][row_num] = {
+            'A': 0,
+            'B': 0,
+            'Score': 0
+        }
+
+
+# populate score_column
+
+for col_num in range(j):
+    for row_num in range((i - 4) + 1):
+        score_column[col_num][row_num] = {
+            'A': 0,
+            'B': 0,
+            'Score': 0
+        }
+
+
+# populate score_diagonal_up_right
+
+for col_num in range((j - 4) + 1):
+    for row_num in range((i - 4) + 1):
+        score_diagonal_up_right[col_num][row_num] = {
+            'A': 0,
+            'B': 0,
+            'Score': 0
+        }
+
+
+# populate score_diagonal_down_right
+
+for col_num in range((j - 4) + 1):
+    for row_num in range((i - 4) + 1):
+        score_diagonal_up_right[col_num][row_num] = {
+            'A': 0,
+            'B': 0,
+            'Score': 0
+        }
+
+
+
+
+
+
+'''
 score = array_1
 for i in range(4):
     for j in range(4):
@@ -31,7 +106,7 @@ for i in range(4):
             'left': 0,
             'up-left': 0
         }
-
+'''
 
 count = 0
 score = 0
